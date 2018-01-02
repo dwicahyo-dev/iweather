061cada89555bbb815a9c9738ce366e03a11579a
dwicahyo-dev <dwoppzxc@gmail.com>
2018-01-03 01:24:05 +0700

    Weather Provider

--- src/providers/weather/weather.ts
+++ src/providers/weather/weather.ts
@@ -7,15 +7,23 @@ export class WeatherProvider {
   apiKey: string = 'eca0c3cce20cd52d';
   url: string;
 
+  weather:any;
 
   constructor(public http: HttpClient) {
     console.log('Hello WeatherProvider Provider');
 
-    this.url = `http://api.wunderground.com/api/${this.apiKey}/conditions/q/CA/San_Francisco.json`;
+    this.url = `http://api.wunderground.com/api/${this.apiKey}/conditions/q/`;
   }
 
-  getWeather(city, state){
-     return this.http.get( )
+  getWeather(city, state) {
+    return new Promise(resolve => {
+      this.http.get(`${this.url}/${state}/${city}.json`).subscribe(data => {
+        resolve(data);
+      }, err => {
+        console.log(err);
+      });
+    });
   }
+
 
 }
