91e063cbc2c3e2322b43eb34c7c907d1c932f5b7
dwicahyo-dev <dwoppzxc@gmail.com>
2018-01-03 01:25:58 +0700

    Home Page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+import { WeatherProvider } from "../../providers/weather/weather";
+
 @IonicPage()
 @Component({
   selector: 'page-home',
@@ -8,7 +10,27 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  weather:any;
+  location:{
+    city:string,
+    state:string
+  };
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private weatherProvider:WeatherProvider) {
+    this.getWeather();
+  }
+
+  getWeather() {
+      this.location= {
+        city: 'Jakarta',
+        state: 'ID'
+      };
+
+      this.weatherProvider.getWeather(this.location.city, this.location.state)
+        .then(data => {
+          this.weather = data.current_observation;
+          console.log(this.weather.display_location.full);
+      });
   }
 
   ionViewDidLoad() {
